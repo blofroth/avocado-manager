aca2b38a19c6a8ddebd951106e34ed820bc4fd12
Björn Löfroth <bjorn.lofroth@ffcg.se>
2019-11-04 20:27:51 +0100

    Add avocado id, allow cors

--- backend/src/main/java/se/ffcg/gkelunch/Avocado.java
+++ backend/src/main/java/se/ffcg/gkelunch/Avocado.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 
 public class Avocado {
 
+    public String id;
     public Integer ripeness;
     public LocalDate dateOfPurchase;
     public String breed;
 
-    public Avocado(Integer ripeness, LocalDate dateOfPurchase, String breed) {
+    public Avocado(String id, Integer ripeness, LocalDate dateOfPurchase, String breed) {
+        this.id = id;
         this.ripeness = ripeness;
         this.dateOfPurchase = dateOfPurchase;
         this.breed = breed;
--- backend/src/main/java/se/ffcg/gkelunch/AvocadoController.java
+++ backend/src/main/java/se/ffcg/gkelunch/AvocadoController.java
@@ -6,10 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Set;
+import java.util.*;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -19,7 +16,7 @@ public class AvocadoController {
     private Set<Avocado> avocados = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
 
     public AvocadoController() {
-        avocados.add(new Avocado(1, LocalDate.of(2019,11,4), "Hass"));
+        avocados.add(new Avocado(UUID.randomUUID().toString(),1, LocalDate.of(2019,11,4), "Hass"));
     }
 
     @GET
