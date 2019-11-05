ba744d18e8b285de63622f38a5cd559a4a189c61
Björn Löfroth <bjorn.lofroth@ffcg.se>
2019-11-05 21:56:10 +0100

    Dummy post build update commit

--- backend/src/main/java/se/ffcg/gkelunch/AvocadoController.java
+++ backend/src/main/java/se/ffcg/gkelunch/AvocadoController.java
@@ -15,6 +15,7 @@ public class AvocadoController {
 
     private Set<Avocado> avocados = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
 
+    /** Initialize in-mem avocado list */
     public AvocadoController() {
         avocados.add(new Avocado(UUID.randomUUID().toString(),1, LocalDate.of(2019,11,4), "Hass"));
         avocados.add(new Avocado(UUID.randomUUID().toString(),1, LocalDate.of(2019,11,3), "Fuerte"));
