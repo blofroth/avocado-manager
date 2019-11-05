dc2da7e26d0b75854d56208cdd3baa6fd819cbb6
Björn Löfroth <bjorn.lofroth@ffcg.se>
2019-11-05 21:16:33 +0100

    Fix uninstall script

--- deployment/uninstall.sh
+++ deployment/uninstall.sh
@@ -3,4 +3,4 @@
 DIR=$(dirname "$0")
 kubectl delete svc avocado-backend avocado-frontend
 kubectl delete -f $DIR/backend.deployment.yaml
-kubectl delete -f $DIRfrontend.deployment.yaml
+kubectl delete -f $DIR/frontend.deployment.yaml
